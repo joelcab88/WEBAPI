1e630bb4cfbd055ae2bc111af2b962070a4e3a25
joelc <joelc@LAPTOP-147FCBJT>
2021-01-31 10:14:04 -0600

    JFCABY - Se agregaa método GetAllProducts para obtener una lista de todos los productos.

--- Models/Methods/GetProducts.cs
+++ Models/Methods/GetProducts.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
@@ -21,6 +23,29 @@ namespace PRODUCTOS_COTEMAR.Models.Methods
         {
             this.UriApi = "http://cotdevsampleapi.azurewebsites.net/v1/"; //API COTEMAR
             this.mediaHeader = new MediaTypeWithQualityHeaderValue("application/json");
+        }
+
+        /// <summary>
+        /// Obtiene una lista de todos los productos.
+        /// </summary>
+        /// <returns>Lista de productos</returns>
+        public async Task<List<ProductViewModel>> GetAllProducts()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                String petition = "products/forsale";
+                client.BaseAddress = new Uri(this.UriApi);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(mediaHeader);
+                HttpResponseMessage respuesta = await client.GetAsync(petition);
+                if (respuesta.IsSuccessStatusCode)
+                {
+                    var jsonResult = await respuesta.Content.ReadAsStringAsync();
+                    var lstResponse = JsonConvert.DeserializeObject<ListProduct>(jsonResult).data;
+                    return lstResponse;
+                }
+                else { return null; }
+            }
         }
     }
 }
