f9667ccfb5961becca2ffc4b6ff299f2ea5952c7
joelc <joelc@LAPTOP-147FCBJT>
2021-01-31 09:54:05 -0600

    JFCABY - Se agrega nuevo controlador y Modelo de datos

---
+++ Controllers/ProuductsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PRODUCTOS_COTEMAR.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProuductsController : ControllerBase
+    {
+        // GET: api/<ProuductsController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ProuductsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ProuductsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ProuductsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProuductsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/ProductViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRODUCTOS_COTEMAR.Models
+{
+    public class ProductViewModel
+    {
+        public int id { get; set; }
+        public double price { get; set; }
+        public string name { get; set; }
+        public int stock { get; set; }
+        public List<object> notifications { get; set; }
+    }
+}
