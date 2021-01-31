6495688ca27cd31b7fd64f97db5be0c015f39dd1
joelc <joelc@LAPTOP-147FCBJT>
2021-01-31 10:00:07 -0600

    JFCABY - Se agregan clases para obtener información de uno o varios productos.

---
+++ Models/ListProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRODUCTOS_COTEMAR.Models
+{
+    /// <summary>
+    /// Clase para obtener el listados de todos los productos.
+    /// </summary>
+    public class ListProduct
+    {
+        public bool success { get; set; }
+        public List<ProductViewModel> data { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRODUCTOS_COTEMAR.Models
+{
+    /// <summary>
+    /// Clase para obtener detalles de un producto.
+    /// </summary>
+    public class Product
+    {
+        public bool success { get; set; }
+        public ProductViewModel data { get; set; }
+    }
+}
