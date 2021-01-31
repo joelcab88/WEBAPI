150416dd7a7648195131dde89a0a3644b351d2fd
joelc <joelc@LAPTOP-147FCBJT>
2021-01-31 09:30:37 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PRODUCTOS_COTEMAR.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
