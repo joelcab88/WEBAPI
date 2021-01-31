75a32b90b16d3557df2d5763b85ffa02eadc47e7
joelc <joelc@LAPTOP-147FCBJT>
2021-01-31 10:06:10 -0600

    JFCABY - Se agrega clase para almacenar métodos que obtienen el detalle de uno varios Productos.

---
+++ Models/Methods/GetProducts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRODUCTOS_COTEMAR.Models.Methods
+{
+    /// <summary>
+    /// Clase para obtener detalle de uno o varios productos.
+    /// </summary>
+    public class GetProducts
+    {
+    }
+}
