82a2d952922781836550e62ae75d1376f3e05e7d
joelc <joelc@LAPTOP-147FCBJT>
2021-01-31 19:33:34 -0600

    JFCABY - Se configura ruta Inicial de proyecto al depurar.

--- Startup.cs
+++ Startup.cs
@@ -50,7 +50,7 @@ namespace PRODUCTOS_COTEMAR
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Products}/{action=Index}");
             });
         }
     }
