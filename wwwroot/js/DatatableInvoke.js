7b0d996307997513478540742d2bb16e1e7b1151
joelc <joelc@LAPTOP-147FCBJT>
2021-01-31 23:34:24 -0600

    JFCABY - Modificaciones de diseño.

---
+++ wwwroot/js/DatatableInvoke.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    var table = $('#example').DataTable({
+    fixedHeader: true,
+    "language": {
+        "url": "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
+     }
+   });
+});
